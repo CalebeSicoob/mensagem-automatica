43633a443b38db6845d721ae9d6519de61ee4421
CalebeSicoob <calebe.hagmayer@segurosicoob.com.br>
2025-10-06 15:20:26 -0300

    automação de abrir a página do wats

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import openpyxl
+from urllib.parse import quote
+import webbrowser
+from time import sleep
+
+webbrowser.open('https://web.whatsapp.com/')
+sleep(30)
 
 planilha = openpyxl.load_workbook('Pasta1.xlsx')
 pagina_clientes = planilha['Planilha1'] # A variável 'workbook' não existe, substitua por 'planilha'
@@ -6,4 +12,12 @@ pagina_clientes = planilha['Planilha1'] # A variável 'workbook' não existe, su
 # Use .iter_rows() para iterar no openpyxl
 for linha in pagina_clientes.iter_rows(min_row=1):
     nome = linha[1].value
-    print(f"nome do cliente: {nome}")
+    telefone = linha[2].value
+    vencimento = linha[3].value
+    mensagem = f'Olá, tudo bem?'
+    print(nome, telefone, vencimento)
+    link_mensagem_wats = f'https://web.whatsapp.com/send?phone={telefone}&text={quote(mensagem)}'
+    webbrowser.open(link_mensagem_wats)
+    input('')
+   
+#dev aprender 
